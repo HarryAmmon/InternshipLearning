5c56d5bc763f869952bdf234c8ecb269ad4f86ee
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-19 17:03:36 +0100

    Updated footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Copyright from './Copyright.js';
+import styles from './Footer.module.css';
+import SocialButton from './SocialButton.js';
 const date= new Date();
 
 export default function Footer(){
     return(
-        <footer>
-            <small>
-            © {date.getFullYear()} Harry Ammon
-            </small>
+        <footer className={styles.root}>
+            <Copyright year={date.getFullYear()} />
+            <SocialButton url="https://github.com/HarryAmmon" icon={faGithub} />
+            <SocialButton url="https://twitter.com/harry_a99" icon ={faTwitter} />
         </footer>
     );
 }
