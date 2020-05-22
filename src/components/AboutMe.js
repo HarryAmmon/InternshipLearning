bd955b4f1a6980c381f6e98acfa1d296f214501c
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-22 11:19:18 +0100

    Not passes props to the Article component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Styles from './AboutMe.module.css';
+import Article from './Article.js';
 
 export default function AboutMe(){
     return(
-        <article className={Styles.root}>
-            <h2>About Me!</h2>
-            <b>👋</b>
-            <p>Hi! I'm Harry and I'm a software developer. I currently am an Intern for The Pensions Regulator and have spent the last year absorbing as much knowledge as possible!</p>
-            <p>Based in South-East England, I can be found cooking, cycling or in a beer garden!</p>
-            <p>I have learnt a lot over the last year and here I will talk about what I have learnt, projects I have been involved with and challenges faced!</p>
-        </article>
+        <Article 
+            title="About Me!"
+            subTitle="👋"
+            paragraphs={[
+                "Hi! I'm Harry and I'm a software developer. I currently am an Intern for The Pensions Regulator and have spent the last year absorbing as much knowledge as possible!",
+                "Based in South-East England, I can be found cooking, cycling or in a beer garden!",
+                "I have learnt a lot over the last year and here I will talk about what I have learnt, projects I have been involved with and challenges faced!"
+            ]}    
+        />   
     );
 }
