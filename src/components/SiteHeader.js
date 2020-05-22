650951814b7ca8128e55d04acee7dbdbea1c1442
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-22 09:37:03 +0100

    Renamed to SiteHeader

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Styles from './Header.module.css';
+import Styles from './SiteHeader.module.css';
+import NavBar from './NavBar';
 
 export default function Header(){
     return(
-        <header className={Styles.root}>
+        <header className={Styles.root}>             
                 <h1>Internship Learning</h1>
                 <p className={Styles.subtitle}>Documenting my learning journey</p>
         </header>
