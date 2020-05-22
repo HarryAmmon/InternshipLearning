264f1e290a526b853dccf5c768fa585253249347
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-22 09:36:19 +0100

    All views are now wrapped in a div

--- src/views/DotNetCorePage.js
+++ src/views/DotNetCorePage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const DotNetCore = _ => <h1>DotNetCore Page :)</h1>;
+const DotNetCore = _ => (
+    <div className="DotNetCorePage">
+        <h1>DotNetCore Page :)</h1>
+    </div>
+);
 
 export default DotNetCore;
