f5ad04ddcda34ff5d9e063b649af357195d66c91
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-22 14:01:44 +0100

    Implementing PageHeaders and Articles

--- src/views/DocumentManagement.js
+++ src/views/DocumentManagement.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import PageHeader from '../components/PageHeader.js';
+import BackgroundImage from '../images/DocManHeaderImage.jpg';
+import Article from '../components/Article.js';
 
 const DocumentManagement = _ => (
     <div className="DocManPage">
-        <h1>DocManPage :)</h1>
+        <PageHeader
+            title = "DocMan"
+            subTitle ="A lesson in refactoring"
+            backgroundImage = {BackgroundImage}
+        />
+        <Article
+            title="Planning Day"
+            subTitle="A sense of scale"
+            paragraphs={['some text','some different text']}
+        />
+        <Article
+            title="SharePoint oAuth"
+            paragraphs={['some more text','writing about the planning day']}
+        /> 
+        <Article
+            title="Creating PoCs"
+            paragraphs={['writing about proof of concepts']}
+        />
+        <Article
+            title="Ways of working"
+            subTitle="Agile & Azure DevOps"
+            paragraphs={['writing about agile','sprint planning','tracking tasks','daily scrum']}
+        />
     </div>
 );
 
