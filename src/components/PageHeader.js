e8c28b2003f125ee9c651c888e075b0d55a69ec0
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-22 13:48:36 +0100

    Created page header component

---
+++ src/components/PageHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './PageHeader.module.css';
+
+export default function PageHeader(props){
+    return(
+        <div 
+            className={Styles.root} 
+            style={{ backgroundImage: `url(${props.backgroundImage})`}}
+        >
+            <h1 className={Styles.title}>{props.title}</h1>
+            <h2 className={Styles.subTitle}>{props.subTitle}</h2>
+        </div>
+    );
+}
