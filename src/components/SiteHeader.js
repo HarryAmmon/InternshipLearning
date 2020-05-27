e7942047caea765611aa99c463f6cd07f2d0b779
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-27 10:38:34 +0100

    Added link to section lower down on page

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Styles from './SiteHeader.module.css';
-import NavBar from './NavBar';
+
 
 export default function Header(){
     return(
         <header className={Styles.root}>             
                 <h1>Internship Learning</h1>
                 <p className={Styles.subtitle}>Documenting my learning journey</p>
+                <a href="#content">#</a>
         </header>
     );
 }
