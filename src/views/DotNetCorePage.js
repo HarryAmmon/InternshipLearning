59670a3b553ab457b05e6d7e4ace2945d7192984
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-28 18:16:54 +0100

    Adding button to show/ hide nav bar

--- src/views/DotNetCorePage.js
+++ src/views/DotNetCorePage.js
@@ -3,16 +3,19 @@ import PageHeader from '../components/PageHeader';
 import backgroundImage from '../images/DotNetPageHeader.jpg'
 import Article from '../components/Article';
 import NavBar from '../components/NavBar';
+import ShowNavBarButton from '../components/ShowNavBarButton.js';
 
 const DotNetCore = _ => (
     <div className="DotNetCorePage">
         <PageHeader 
-            title="Front End"
-            subTitle="JavaScript, React & CSS"
+            title=".NET Core"
+            subTitle="Console, WebAPI & MVC apps'"
             backgroundImage={backgroundImage}
         />
         <hr/>
         <NavBar />
+        {/* The state of the Nav bar needs to be updated by the button. When the button is clicked it should call a function that changes the class of the navbar */}
+        <ShowNavBarButton /> 
         <Article
             title="An intro to .NET Core"
             paragraphs={["talking about .NET Core","say about what different to .NET Framework"]}
