dcef3cfe23b9e64c618ab1479662d3690eeffeac
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-28 18:18:38 +0100

    NavBar is now a list of Link components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Styles from "./NavBar.module.scss";
+
+
 
 export default function NavBar(){
     return(
         <nav className={Styles.root}>
-            <NavLink to="/" exact >Home</NavLink>
-            <NavLink to="/DotNetCore" >DotNetCore</NavLink>
-            <NavLink to="/DocMan" >DocMan</NavLink>
-            <NavLink to="/FrontEnd">FrontEnd</NavLink>
+            <ul>
+                <li><Link to="/">About Me</Link></li>
+                <li><Link to="/DotNetCore" >.NET Core</Link></li>
+                <li><Link to="/DocMan" >Document Management</Link></li>
+                <li><Link to="/FrontEnd">Front-end</Link></li>
+            </ul>
         </nav>
     );
 }
