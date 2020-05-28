d2a2705d6c66b7c92f215ce6b5dc1ffe888e2009
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-05-28 14:40:03 +0100

    Created another article entry

--- src/views/DotNetCorePage.js
+++ src/views/DotNetCorePage.js
@@ -17,6 +17,11 @@ const DotNetCore = _ => (
             title="An intro to .NET Core"
             paragraphs={["talking about .NET Core","say about what different to .NET Framework"]}
         />
+        <Article
+            title="Coding Kata"
+            subTitle="Bowling"
+            paragraphs={["Coding katas are completed to help hone your programming skills. They are talked about by Robert Martin in 'The Clean Coder'.","I completed the Bowling Kata"]}
+        />
     </div>
 );
 
