fe08b32894ce03fe6553f87916ae1a52378ff196
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-12 12:26:10 +0100

    Adding lines to test code component

--- src/views/FrontEnd.jsx
+++ src/views/FrontEnd.jsx
@@ -3,6 +3,7 @@ import PageHeader from '../components/PageHeader.jsx';
 import Card from '../components/Card.jsx';
 import Styles from './Content.module.scss';
 import Article from '../components/Article.jsx';
+import Code from '../components/Code.jsx';
 
 const FrontEnd = props => (
     <div className="frontEndPage">
@@ -17,12 +18,30 @@ const FrontEnd = props => (
                 </Article>
             </Card>
             <Card>
-                <Article title="Big Picture" subTitle="Assume nothing">
-                    <p>The first stop in my tour of web development is some big picture stuff.</p>
+                <Article title="Big Picture" subTitle="Assume Nothing">
+                    <p>The first stop in my web development tour is some big picture stuff covering the very basics of HTML, CSS & JavaScript.</p>
+                    <p>HTML is a markup language used to define the structure of web page. It use an XML like syntax. The current version of HTML is HTML5 released in 2015. This file is served by servers to your web browser.</p>
+                    <p>CSS is a language used to style HTML documents. It uses various CSS selectors to target HTML elements and change how they appear. The CSS document is referred to as the stylesheet. A reference to the stylesheet must be added in the HTML document. Both HTML and CSS are declarative languages, we tell it what to do, not how to do it.</p>
+                    <p>JavaScript is a imperative, interpreted programming language, we tell it what to do with code which is interpreted at run-time by the browser. JavaScript is used to add functionality to a website. A script tag should be added to the HTML file.</p>
+
+                </Article>
+            </Card>
+            <Card>
+                <Article title="HTML5">
+                    <p>This is some text and that should be shown inline</p>
+                    <p>Here is some more text</p>
+                    <Code>
+                        {`code needs to be shown 
+like this
+        there should be many lines
+        
+        
+        
+        itshouldnt matter how many lines I add`}</Code>
                 </Article>
             </Card>
         </div>
     </div>
 );
 
-export default FrontEnd
+export default FrontEnd;
