6698eb3fe6eb9a903c8d5f817dff3acac3fdd8c9
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-12 12:27:53 +0100

    Component and styling for CodeComponent

---
+++ src/components/Code.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Styles from './Code.module.scss';
+
+export default function Code({ children }){
+    console.log(`Children number: ${React.Children.count(children)}`);
+
+    return(
+        <code className={Styles.root}>
+           {React.Children.map(children, child => {
+                let lineArray = child.split('\n');
+
+                lineArray.forEach((element,i) => {
+                    lineArray[i] = 
+                    <div key={i} className={Styles.line}>
+                        <div className={Styles.lineNumber}>{i + 1}</div>
+                        <div className={Styles.code}>{element}</div>
+                    </div>
+                });
+
+                return(<pre>{lineArray}</pre>)
+           } )}
+        </code>
+    )
+}
+
+
