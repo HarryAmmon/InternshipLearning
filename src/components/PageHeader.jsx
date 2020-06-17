d73c9bda87a537d7a9e53c708179d20929bd5510
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-17 12:31:43 +0100

    Adding margin to PageHeader content

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -7,12 +7,12 @@ export default function PageHeader(props){
     return(
         <div>
             <div className={Styles.root}>
-            <h1 className={Styles.title}>{props.title}</h1>
-            <h2 className={Styles.subTitle}>{props.subTitle}</h2>
+                <h1 className={Styles.title}>{props.title}</h1>
+                <h2 className={Styles.subTitle}>{props.subTitle}</h2>
                 <MenuButton onClick={props.toggleNavBar}/>
             </div>
-            <hr/>
-            <NavBar display={props.displayNav} hideNavBar={props.hideNavBar} />         
+        <hr/>
+        <NavBar display={props.displayNav} hideNavBar={props.hideNavBar} />         
         </div>
     );
 }
