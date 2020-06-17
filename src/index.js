48e4f596908cfeaae21cde7cc5d005cf0b1e2bcd
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-17 12:17:08 +0100

    Scrolls to top when link clicked2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop.jsx';
 const app = document.querySelector("#app");
 
 ReactDom.render(
     <Router>
-    <App />
+        <ScrollToTop/>
+        <App />
     </Router>, app);
