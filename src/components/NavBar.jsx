4da7cef521f8bb24bdc80e509f6096eaea0a9ee3
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-18 10:36:57 +0100

    Setting useState and determing style based on state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import Styles from "./NavBar.module.scss";
 
 
 
 export default function NavBar(props){
+    
+    const [display, setDisplay] = useState(false);
+
+    
     return(
-    <nav className={props.display? Styles.root: Styles.hidden}>
+    <nav className={display ? Styles.root : Styles.hidden} >
         <ul>
             <li><Link to="/" onClick={props.hideNavBar}>About Me</Link></li>
             <li><Link to="/DotNetCore" onClick={props.hideNavBar}>.NET Core</Link></li>
