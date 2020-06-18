e95fb8b8529d7b7546321cf22fadfbc0f02955c0
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-18 11:09:28 +0100

    Refrencing function instead of calling it

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,19 +4,29 @@ import Styles from "./NavBar.module.scss";
 
 
 
-export default function NavBar(props){
+export default function NavBar(){
     
-    const [display, setDisplay] = useState(false);
+    const [display, setDisplay] = useState(true);
 
-    
+
+    useEffect(() => {
+        console.log(`NavBar mounted `);
+        return () => {
+            console.log(`Navbar unmounted `);
+        }
+    },[display]);
+
     return(
-    <nav className={display ? Styles.root : Styles.hidden} >
-        <ul>
-            <li><Link to="/" onClick={setDisplay(false)}>About Me</Link></li>
-            <li><Link to="/DotNetCore" onClick={setDisplay(false)}>.NET Core</Link></li>
-            <li><Link to="/DocMan" onClick={setDisplay(false)}>Document Management</Link></li>
-            <li><Link to="/FrontEnd" onClick={setDisplay(false)}>Front-end</Link></li>
-        </ul>
-    </nav>
+    <div>
+        <nav className={display ? Styles.root : Styles.hidden} >
+            <ul>
+                <li><Link to="/" onClick={() => {setDisplay(false); console.log("onClick Link: '/' pressed")}}>About Me</Link></li>
+                <li><Link to="/DotNetCore" onClick={() => setDisplay(false)}>.NET Core</Link></li>
+                <li><Link to="/DocMan" onClick={() => setDisplay(false)}>Document Management</Link></li>
+                <li><Link to="/FrontEnd" onClick={() => setDisplay(false)}>Front-end</Link></li>
+            </ul>
+        </nav>
+        <button onClick={() => setDisplay(display ? false : true)}>Press me!</button>
+    </div>
     );
 }
