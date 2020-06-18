84d4159db96b068cbc1e52b367f843f3f987f1a7
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-18 16:07:10 +0100

    NavBar gets and sets its state from App Context

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,22 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 import Styles from "./NavBar.module.scss";
+import AppContext from "./AppContext.jsx";
 
-
-
-export default function NavBar(){
-    
-    const [display, setDisplay] = useState(true);
+export default function NavBar(){    
+    const context = useContext(AppContext);
+    const [displayNav, setDisplayNav] = context.displayNavigation;
 
     return(
-    <div>
-        <nav className={display ? Styles.root : Styles.hidden} >
-            <ul>
-                <li><Link to="/" onClick={() => {setDisplay(false); console.log("onClick Link: '/' pressed")}}>About Me</Link></li>
-                <li><Link to="/DotNetCore" onClick={() => setDisplay(false)}>.NET Core</Link></li>
-                <li><Link to="/DocMan" onClick={() => setDisplay(false)}>Document Management</Link></li>
-                <li><Link to="/FrontEnd" onClick={() => setDisplay(false)}>Front-end</Link></li>
-            </ul>
-        </nav>
-        <button onClick={() => setDisplay(state => !state)}>Press me!</button>
-    </div>
+        <div>
+            <nav className={displayNav ? Styles.root : Styles.hidden} >
+                <ul>
+                    <li><Link to="/" onClick={() => {setDisplayNav(false)}}>About Me</Link></li>
+                    <li><Link to="/DotNetCore" onClick={() => setDisplayNav(false)}>.NET Core</Link></li>
+                    <li><Link to="/DocMan" onClick={() => setDisplayNav(false)}>Document Management</Link></li>
+                    <li><Link to="/FrontEnd" onClick={() => setDisplayNav(false)}>Front-end</Link></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
