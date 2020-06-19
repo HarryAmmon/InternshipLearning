7d19ae5b651f993fb0898e35c944014ee80d2437
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-19 16:47:21 +0100

    Reformatting

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Styles from './List.module.scss';
 
-const List = ({Children}) => (
-    <ul>
-        {React.Children.map(Children, (child, index) => 
-            (<li key={index}>{child}</li>)
+const List = ({ listItems }) => {
+    console.log(`No List Items: ${listItems.length}`)
+    console.log(listItems);
+    return(<ul className={Styles.root}>
+        {listItems.map((item, index) => 
+            (<li className={Styles.listItem} key={index}>{item}</li>)
         )}
-    </ul>
-);
+    </ul>)
+};
 
 export default List
