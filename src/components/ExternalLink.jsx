01c0f715f0ca06e0d29caaa25a9cb2d86c5f2859
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-06-23 11:34:39 +0100

    External links now open in new tab

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Styles from "./ExternalLink.module.scss"
 
 export default function ExternalLink(props){
-    return(<a className={Styles.root} href={props.link}>{props.children}</a>)
+    return(<a className={Styles.root} href={props.link} target="_blank">{props.children}</a>)
 }
